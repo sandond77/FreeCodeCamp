eadf098126d35fc74d9ff8bcbc068ecedf99e0bb
sandond77 <sandu@ucdavis.edu>
2018-08-12 15:28:33 -0700

    added solution and test cases for temperature.js

--- algorithms/temperature.js
+++ algorithms/temperature.js
@@ -4,3 +4,22 @@
 // You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature. Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
 
 // Don't worry too much about the function and return statements as they will be covered in future challenges. For now, only use operators that you have already learned.
+
+
+// convertToF(0) should return a number
+// convertToF(-30) should return a value of -22
+// convertToF(-10) should return a value of 14
+// convertToF(0) should return a value of 32
+// convertToF(20) should return a value of 68
+// convertToF(30) should return a value of 86
+
+function convertToF(celsius) {
+	let fahrenheit = (celsius * (9/5)) + 32;
+	return fahrenheit;
+  }
+  
+console.log(convertToF(0));
+console.log(convertToF(-30));
+console.log(convertToF(-10));
+console.log(convertToF(20));
+console.log(convertToF(30));
