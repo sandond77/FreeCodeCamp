1cd81b3d9fbb3d6ddf10cb809e8a89ebcd773f96
sandond77 <sandu@ucdavis.edu>
2018-08-12 22:08:38 -0700

    finished my solution

--- algorithms/largestNumber.js
+++ algorithms/largestNumber.js
@@ -12,22 +12,26 @@
 // }
 
 // largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
-
-
-
-// TEST CASES
-// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) //should return an array.
-// largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) //should return [27, 5, 39, 1001].
-// largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) //should return [9, 35, 97, 1000000].
-// largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]) //should return [25, 48, 21, -3].
-
 function largestOfFour(arr) {
-  for (var i = 0; i < arr.length; i++) {
-  	for (var j = 0; j < arr[i].length; i++) {
-  		console.log(arr[i].[j]);
+	let result = [];
+  	for (var i = 0; i < arr.length; i++) {
+  		let k = arr[i][0];
+  		for (var j = 0; j < arr[i].length; j++) {
+  			// console.log(`the value of k is ${k}`)
+  			// console.log(arr[i][j])
+  			if ( arr[i][j] > k) {
+  				k = arr[i][j]; 
+  			}
+  		}
+		result.push(k)
   	}
-  }
-  return arr;
+  	console.log(result)
+  	return result;
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+// TEST CASES
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) //should return [27, 5, 39, 1001].
+largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) //should return [9, 35, 97, 1000000].
+largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]) //should return [25, 48, 21, -3].
+
