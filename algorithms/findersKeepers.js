f63c3cad4900f21480e8c356a9c8de5eb78d9d51
sandond77 <sandu@ucdavis.edu>
2018-08-14 15:48:39 -0700

    attempting to solve findersKeepersjs

--- algorithms/findersKeepers.js
+++ algorithms/findersKeepers.js
@@ -14,3 +14,12 @@
 
 // findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }) // should return 8.
 // findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }) // should return undefined.
+
+function findElement(arr, func) {
+	for (let i=0, i < arr.length, i++){
+		if (arr[i] % 2)
+	}		
+	return num;
+  	}
+  
+  	findElement([1, 2, 3, 4], num => num % 2 === 0);
