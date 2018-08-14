ae26f7b02e791360c9cadfeeb4dbba5672b9d306
sandond77 <sandu@ucdavis.edu>
2018-08-13 21:21:01 -0700

    added base code, prompt, and test case for stringRepeat.js

---
+++ algorithms/stringRepeat.js
@@ -0,0 +1,21 @@
+// Basic Algorithm Scripting: Repeat a String Repeat a String
+// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+// BASE CODE
+// function repeatStringNumTimes(str, num) {
+//   // repeat after me
+//   return str;
+// }
+
+// repeatStringNumTimes("abc", 3);
+
+// TEST CASES
+// repeatStringNumTimes("*", 3) should return "***".
+// repeatStringNumTimes("abc", 3) should return "abcabcabc".
+// repeatStringNumTimes("abc", 4) should return "abcabcabcabc".
+// repeatStringNumTimes("abc", 1) should return "abc".
+// repeatStringNumTimes("*", 8) should return "********".
+// repeatStringNumTimes("abc", -2) should return "".
+// The built-in repeat()-method should not be used
