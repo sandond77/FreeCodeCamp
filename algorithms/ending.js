6f6099111f52b6ce0a1f4de6e0feb1ffc538bc27
sandond77 <sandu@ucdavis.edu>
2018-08-13 21:15:54 -0700

    added my solution for ending.js

--- algorithms/ending.js
+++ algorithms/ending.js
@@ -23,3 +23,27 @@
 // confirmEnding("Open sesame", "game") //should return false.
 // confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain") //should return false.
 // confirmEnding("Abstraction", "action") //should return true.
+
+
+function confirmEnding(str, target) {
+	if (str.substr(-target.length) === target){	//adding the negative will start the search from opposite end
+		// console.log('\n' + str.substr(-target.length)) 
+		// console.log(target)
+		// console.log(typeof str.substr(-target.length))
+		return console.log(true);
+	} else {
+		// console.log('\n' + str.substr(-target.length))
+		return console.log(false);
+	}
+}
+
+confirmEnding("Bastian", "n") //should return true.
+confirmEnding("Congratulation", "on")// should return true.
+confirmEnding("Connor", "n")// should return false.
+confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification") //should return false.
+confirmEnding("He has to give me a new name", "name") //should return true.
+confirmEnding("Open sesame", "same")// should return true.
+confirmEnding("Open sesame", "pen") //should return false.
+confirmEnding("Open sesame", "game") //should return false.
+confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain") //should return false.
+confirmEnding("Abstraction", "action") //should return true.
