72a318fb6cdfc6a15f8e20fbdb9fa46b990e0f25
sandond77 <sandu@ucdavis.edu>
2018-08-13 21:18:49 -0700

    added two solutions to ending.js

--- algorithms/ending.js
+++ algorithms/ending.js
@@ -37,13 +37,19 @@ function confirmEnding(str, target) {
 	}
 }
 
-confirmEnding("Bastian", "n") //should return true.
-confirmEnding("Congratulation", "on")// should return true.
-confirmEnding("Connor", "n")// should return false.
-confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification") //should return false.
-confirmEnding("He has to give me a new name", "name") //should return true.
-confirmEnding("Open sesame", "same")// should return true.
-confirmEnding("Open sesame", "pen") //should return false.
-confirmEnding("Open sesame", "game") //should return false.
-confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain") //should return false.
-confirmEnding("Abstraction", "action") //should return true.
+// Solution #1
+// function confirmEnding(string, target) {
+//   if (string.substr(-target.length) === target) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// confirmEnding('Bastian', 'n');
+
+
+// Solution #2
+// function confirmEnding(string, target) {
+//   return (string.substr(-target.length) === target) ? true : false;
+// }
+// confirmEnding('Bastian', 'n');
