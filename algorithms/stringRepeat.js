2ea9b5563f419952970ac69c758860dea733b9c5
sandond77 <sandu@ucdavis.edu>
2018-08-13 21:32:47 -0700

    added my solution to stringRepeat.js

--- algorithms/stringRepeat.js
+++ algorithms/stringRepeat.js
@@ -19,3 +19,21 @@
 // repeatStringNumTimes("*", 8) should return "********".
 // repeatStringNumTimes("abc", -2) should return "".
 // The built-in repeat()-method should not be used
+
+function repeatStringNumTimes(str, num) {
+	var holder = [];
+	for (var i = 0; i < num; i++) {
+		holder.push(str);
+	}
+	holder = holder.join("");
+	return console.log(holder);
+}
+
+repeatStringNumTimes("abc", 3);
+repeatStringNumTimes("*", 3) //should return "***".
+repeatStringNumTimes("abc", 3) //should return "abcabcabc".
+repeatStringNumTimes("abc", 4) //should return "abcabcabcabc".
+repeatStringNumTimes("abc", 1) //should return "abc".
+repeatStringNumTimes("*", 8) //should return "********".
+repeatStringNumTimes("abc", -2) //should return "".
+
