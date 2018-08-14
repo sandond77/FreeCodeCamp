f9adeb8c842d2a7b2442a43f8023f82d1c08cb3f
sandond77 <sandu@ucdavis.edu>
2018-08-13 22:00:35 -0700

    added solutions to truncateString.js

--- algorithms/truncateString.js
+++ algorithms/truncateString.js
@@ -19,6 +19,7 @@
 // truncateString("A-", 1) //should return "A...".
 // truncateString("Absolutely Longer", 2) //should return "Ab...".
 
+// My Solution #1
 function truncateString(str, num) {
   // Clear out that junk in your trunk
   if (str.length > num) {
@@ -34,19 +35,20 @@ function truncateString(str, num) {
   }
 }
 
-// function truncateString(str, num) {
-//   // Clear out that junk in your trunk
-//   	// console.log(str.length)
-// 	if (str.length > num) {
-// 		str = str.slice(0,num);
-// 		str = str.concat("...");
-// 		console.log(str);
-// 		return str;
-// 	} else {
-// 		console.log(str);
-// 	  	return str;
-// 	}
-// }
+// My Solution #2
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  	// console.log(str.length)
+	if (str.length > num) {
+		str = str.slice(0,num);
+		str = str.concat("...");
+		console.log(str);
+		return str;
+	} else {
+		console.log(str);
+	  	return str;
+	}
+}
 
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8) //should return "A-tisket...".
@@ -55,3 +57,27 @@ truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket
 truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2) //should return "A-tisket a-tasket A green and yellow basket".
 truncateString("A-", 1) //should return "A...".
 truncateString("Absolutely Longer", 2) //should return "Ab...".
+
+
+
+// GIVEN SOLUTIONS 1
+
+// function truncateString(str, num) {
+//   // Clear out that junk in your trunk
+//   if (str.length > num && num > 3) {
+//     return str.slice(0, (num - 3)) + '...';
+//   } else if (str.length > num && num <= 3) {
+//     return str.slice(0, num) + '...';
+//   } else {
+//     return str;
+//   }
+// }
+
+// GIVEN SOLUTIONS 2
+// function truncateString(str, num) {
+//   if (str.length <= num) {
+//     return str;
+//   } else {
+//     return str.slice(0, num > 3 ? num - 3 : num) + '...';
+//   }
+// }
