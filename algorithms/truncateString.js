ed8f5174544526df5592ed8411275817c63b7f51
sandond77 <sandu@ucdavis.edu>
2018-08-13 21:59:00 -0700

    added my two solutions to truncateString.js

--- algorithms/truncateString.js
+++ algorithms/truncateString.js
@@ -18,3 +18,40 @@
 // truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2) //should return "A-tisket a-tasket A green and yellow basket".
 // truncateString("A-", 1) //should return "A...".
 // truncateString("Absolutely Longer", 2) //should return "Ab...".
+
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (str.length > num) {
+  	str = str.split("").reverse().join("")
+	str = str.substr((str.length-num));
+	str = str.split("").reverse().join("")
+	str = str.concat("...")
+	console.log(str)
+	return str;
+  } else {
+  	console.log(str)
+  	return str;
+  }
+}
+
+// function truncateString(str, num) {
+//   // Clear out that junk in your trunk
+//   	// console.log(str.length)
+// 	if (str.length > num) {
+// 		str = str.slice(0,num);
+// 		str = str.concat("...");
+// 		console.log(str);
+// 		return str;
+// 	} else {
+// 		console.log(str);
+// 	  	return str;
+// 	}
+// }
+
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8) //should return "A-tisket...".
+truncateString("Peter Piper picked a peck of pickled peppers", 11) //should return "Peter Piper...".
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length) //should return "A-tisket a-tasket A green and yellow basket".
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2) //should return "A-tisket a-tasket A green and yellow basket".
+truncateString("A-", 1) //should return "A...".
+truncateString("Absolutely Longer", 2) //should return "Ab...".
