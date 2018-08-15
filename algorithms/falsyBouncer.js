7f98853fe013dc056def4e6fb4766687abef8099
sandond77 <sandu@ucdavis.edu>
2018-08-15 15:55:18 -0700

    added prompt, base code, and test code falsyBouncer

---
+++ algorithms/falsyBouncer.js
@@ -0,0 +1,29 @@
+// Basic Algorithm Scripting: Falsy Bouncer
+// Remove all falsy values from an array.
+
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+// Hint: Try converting each value to a Boolean.
+
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+// BASE CODE
+// function bouncer(arr) {
+// 	// Don't show a false ID to this bouncer.
+// 	return arr;
+//   }
+  
+//   bouncer([7, "ate", "", false, 9]);
+
+// TEST CODE
+// bouncer([7, "ate", "", false, 9]) //should return [7, "ate", 9].
+// bouncer(["a", "b", "c"]) //should return ["a", "b", "c"].
+// bouncer([false, null, 0, NaN, undefined, ""]) //should return [].
+// bouncer([1, null, NaN, 2, undefined]) //should return [1, 2].
+
+function bouncer(arr) {
+	// Don't show a false ID to this bouncer.
+	return arr;
+  }
+  
+bouncer([7, "ate", "", false, 9]);
