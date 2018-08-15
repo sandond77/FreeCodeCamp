5d8f26971ee9087704d3602ec13cd3d38c3f33b9
sandond77 <sandu@ucdavis.edu>
2018-08-15 16:07:28 -0700

    added my solution to falsyBouncer.js:

--- algorithms/falsyBouncer.js
+++ algorithms/falsyBouncer.js
@@ -23,7 +23,12 @@
 
 function bouncer(arr) {
 	// Don't show a false ID to this bouncer.
+	arr = arr.filter(Boolean)
+	console.log(arr)
 	return arr;
   }
   
 bouncer([7, "ate", "", false, 9]);
+bouncer(["a", "b", "c"]) //should return ["a", "b", "c"].
+bouncer([false, null, 0, NaN, undefined, ""]) //should return [].
+bouncer([1, null, NaN, 2, undefined]) //should return [1, 2].
