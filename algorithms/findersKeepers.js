300e55f8b66cb997a1c6bd674d740f2301b7b1e2
sandond77 <sandu@ucdavis.edu>
2018-08-14 21:37:29 -0700

    completed my solution and verified it works

--- algorithms/findersKeepers.js
+++ algorithms/findersKeepers.js
@@ -16,10 +16,14 @@
 // findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }) // should return undefined.
 
 function findElement(arr, func) {
-	for (let i=0, i < arr.length, i++){
-		if (arr[i] % 2)
-	}		
-	return num;
-  	}
-  
-  	findElement([1, 2, 3, 4], num => num % 2 === 0);
+	for (let i=0; i < arr.length; i++){
+		let num = arr[i]
+		if (func(num) === true) {
+			return console.log(num)
+		} 
+	}	
+}
+
+// findElement([1, 2, 3, 4], num => num % 2 === 0);
+findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }) // should return 8.
+// findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }) // should return undefined.
