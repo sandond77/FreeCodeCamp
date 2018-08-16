818741f62e935f777148231aa03abe56056bc523
sandond77 <sandu@ucdavis.edu>
2018-08-16 14:46:19 -0700

    added solutions to sumRange.js

--- algorithms/Intermediate/sumRange.js
+++ algorithms/Intermediate/sumRange.js
@@ -18,7 +18,7 @@
 // sumAll([10, 5]) //should return 45.
 
 
-
+// MY SOLUTION
 function sumAll(arr) {
 	let min = Math.min(arr[0],arr[1]);
 	let max = Math.max(arr[0],arr[1]);
@@ -34,3 +34,38 @@ sumAll([1, 4]) //should return 10.
 sumAll([4, 1]) //should return 10.
 sumAll([5, 10]) //should return 45.
 sumAll([10, 5]) //should return 45.
+
+
+// GIVEN SOLUTION #1
+// function sumAll(arr) {
+//     var max = Math.max(arr[0], arr[1]);
+//     var min = Math.min(arr[0], arr[1]);
+//     var temp = 0;
+//     for (var i=min; i <= max; i++){
+//         temp += i;
+//     }
+//   return(temp);
+// }
+
+// GIVEN SOLUTION #2
+// function sumAll(arr) {
+// 	// Buckle up everything to one!
+  
+// 	// Using ES6 arrow function (one-liner)
+// 	var sortedArr = arr.sort((a,b) => a-b);
+// 	var firstNum = arr[0];
+// 	var lastNum = arr[1];
+// 	// Using Arithmetic Progression summing formula
+  
+// 	var sum = (lastNum - firstNum + 1) * (firstNum + lastNum) / 2;
+// 	return sum;
+//   }
+
+// GIVEN SOLUTION #3
+// function sumAll(arr) {
+//     var sum = 0;
+//     for (var i = Math.min(...arr); i <= Math.max(...arr); i++){
+//         sum += i;
+//     }
+//   return sum;
+// }
