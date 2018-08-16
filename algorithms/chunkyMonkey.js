2c3ea56eef2246407f1019ae94c0272ca390db0e
sandond77 <sandu@ucdavis.edu>
2018-08-15 17:33:08 -0700

    Added 3 solutions for chunkymonkey js:

--- algorithms/chunkyMonkey.js
+++ algorithms/chunkyMonkey.js
@@ -47,3 +47,49 @@ chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) //should return [[0, 1, 2, 3], [4, 5]]
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) //should return [[0, 1, 2], [3, 4, 5], [6]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) //should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) //should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
+
+
+// SOLUTION 1
+// function chunkArrayInGroups(arr, size) {
+
+// 	var temp = [];
+// 	var result = [];
+
+// 	for (var a = 0; a < arr.length; a++) {
+// 	  if (a % size !== size - 1)
+// 		temp.push(arr[a]);
+// 	  else {
+// 		temp.push(arr[a]);
+// 		result.push(temp);
+// 		temp = [];
+// 	  }
+// 	}
+
+// 	if (temp.length !== 0)
+// 	  result.push(temp);
+// 	return result;
+//   }
+
+// SOLUTION 2
+// function chunkArrayInGroups(arr, size) {
+// 	// Break it up.
+// 	var arr2 = [];
+// 	for (var i = 0; i < arr.length; i+=size) {
+// 	  arr2.push(arr.slice(i , i+size));
+// 	}
+// 	return arr2;
+//   }
+
+// SOLUTION 3
+// function chunkArrayInGroups(arr, size) {
+// 	// Break it up.
+// 	var newArr = [];
+// 	var i = 0;
+
+// 	while (i < arr.length) {
+// 	  newArr.push(arr.slice(i, i+size));
+// 	  i += size;
+// 	}
+// 	return newArr;
+//   }
+//   chunkArrayInGroups(["a", "b", "c", "d"], 2);
