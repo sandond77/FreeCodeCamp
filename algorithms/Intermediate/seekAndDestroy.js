cd6faf77d945f9145bd9925db7377c3f9ad6137e
sandond77 <sandu@ucdavis.edu>
2018-08-17 10:34:14 -0700

    added base code, prompt, and test code

---
+++ algorithms/Intermediate/seekAndDestroy.js
@@ -0,0 +1,31 @@
+// Intermediate Algorithm Scripting: Seek and Destroy
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+// Note
+// You have to use the arguments object.
+
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+// BASE CODE
+// function destroyer(arr) {
+// 	// Remove all the values
+// 	return arr;
+//   }
+  
+//   destroyer([1, 2, 3, 1, 2, 3], 2, 3);	 
+
+// TEST CODE
+// destroyer([1, 2, 3, 1, 2, 3], 2, 3) should return [1, 1].
+// destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) should return [1, 5, 1].
+// destroyer([3, 5, 1, 2, 2], 2, 3, 5) should return [1].
+// destroyer([2, 3, 2, 3], 2, 3) should return [].
+// destroyer(["tree", "hamburger", 53], "tree", 53) should return ["hamburger"].
+// destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan") should return [12,92,65].
+
+
+  function destroyer(arr) {
+	// Remove all the values
+	return arr;
+  }
+  
+  destroyer([1, 2, 3, 1, 2, 3], 2, 3);
