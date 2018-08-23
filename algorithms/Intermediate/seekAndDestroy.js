f81439970ae2bbba0fdddd6097018b1a9bc6f592
sandond77 <sandu@ucdavis.edu>
2018-08-22 21:38:50 -0700

    added given solutions for seekAndDestroy.js

--- algorithms/Intermediate/seekAndDestroy.js
+++ algorithms/Intermediate/seekAndDestroy.js
@@ -15,14 +15,14 @@
 //   destroyer([1, 2, 3, 1, 2, 3], 2, 3);	 
 
 // TEST CODE
-// destroyer([1, 2, 3, 1, 2, 3], 2, 3) should return [1, 1].
-// destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) should return [1, 5, 1].
-// destroyer([3, 5, 1, 2, 2], 2, 3, 5) should return [1].
-// destroyer([2, 3, 2, 3], 2, 3) should return [].
-// destroyer(["tree", "hamburger", 53], "tree", 53) should return ["hamburger"].
-// destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan") should return [12,92,65].
+// destroyer([1, 2, 3, 1, 2, 3], 2, 3) //should return [1, 1].
+// destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) //should return [1, 5, 1].
+// destroyer([3, 5, 1, 2, 2], 2, 3, 5) //should return [1].
+// destroyer([2, 3, 2, 3], 2, 3) //should return [].
+// destroyer(["tree", "hamburger", 53], "tree", 53) //should return ["hamburger"].
+// destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan") //should return [12,92,65].
 
-
+//MY SOLUTION
 function destroyer(arr) {
 
 	let arg = Array.prototype.slice.call(arguments);
@@ -38,6 +38,32 @@ function destroyer(arr) {
 	return arr;
 }
 
+destroyer([1, 2, 3, 1, 2, 3], 2, 3)// should return [1, 1].
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) //should return [1, 5, 1].
+destroyer([3, 5, 1, 2, 2], 2, 3, 5) //should return [1].
+destroyer([2, 3, 2, 3], 2, 3) //should return [].
+destroyer(["tree", "hamburger", 53], "tree", 53) //should return ["hamburger"].
+destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan") //should return [12,92,65].
 
-  
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+// GIVEN SOLUTION #1
+// function destroyer(arr) {
+//   var args = Array.prototype.slice.call(arguments);
+
+//   for (var i = 0; i < arr.length; i++) {
+//     for (var j = 0; j < args.length; j++) {
+//       if (arr[i] === args[j]) {
+//         delete arr[i];
+//       }
+//     }
+//   }
+//   return arr.filter(Boolean);
+// }
+
+// GIVEN SOLUTION #2
+// function destroyer(arr) {
+//   var args = Array.from(arguments).slice(1);
+//   return arr.filter(function(val) {
+//     return !args.includes(val);
+//   });
+// }
+
