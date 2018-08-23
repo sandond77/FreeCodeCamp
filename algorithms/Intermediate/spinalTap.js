13a41cecbd7e4baa7a7750271e5f88aa79cd0198
sandond77 <sandu@ucdavis.edu>
2018-08-23 14:57:22 -0700

    added first attempt at spinalTap.js

--- algorithms/Intermediate/spinalTap.js
+++ algorithms/Intermediate/spinalTap.js
@@ -21,9 +21,13 @@
 // spinalCase("AllThe-small Things") //should return "all-the-small-things".
 
 function spinalCase(str) {
-// "It's such a fine line between stupid, and clever."
-// --David St. Hubbins
+str = str.toLowerCase().split(" ").join("-");
+console.log(str)
 return str;
 }
 
 spinalCase('This Is Spinal Tap');
+spinalCase("thisIsSpinalTap") //should return "this-is-spinal-tap".
+spinalCase("The_Andy_Griffith_Show") //should return "the-andy-griffith-show".
+spinalCase("Teletubbies say Eh-oh")// should return "teletubbies-say-eh-oh".
+spinalCase("AllThe-small Things") //should return "all-the-small-things".
