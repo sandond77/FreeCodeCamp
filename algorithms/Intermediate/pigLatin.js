c65641b692147573c69c577e4a89cddbdd6062b9
sandond77 <sandu@ucdavis.edu>
2018-08-25 14:27:05 -0700

    added given solutions for pigLatin.js

--- algorithms/Intermediate/pigLatin.js
+++ algorithms/Intermediate/pigLatin.js
@@ -54,3 +54,68 @@ translatePigLatin("glove") //should return "oveglay".
 translatePigLatin("algorithm") //should return "algorithmway".
 translatePigLatin("eight") //should return "eightway".
 translatePigLatin("TV")
+
+
+
+
+// BASIC SOLUTION
+// function translatePigLatin(str) {
+//   // Create variables to be used
+//   var pigLatin = '';
+//   var regex = /[aeiou]/gi;
+
+//   // Check if the first character is a vowel
+//   if (str[0].match(regex)) {
+//     pigLatin = str + 'way';
+
+//   } else if(str.match(regex) === null) {
+//     // Check if the string contains only consonants
+//     pigLatin = str + 'ay';
+//   } else {
+
+//     // Find how many consonants before the first vowel.
+//     var vowelIndice = str.indexOf(str.match(regex)[0]);
+
+//     // Take the string from the first vowel to the last char
+//     // then add the consonants that were previously omitted and add the ending.
+//     pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + 'ay';
+//   }
+
+//   return pigLatin;
+// }
+
+// INTERMEDIATE SOLUTION
+// function translatePigLatin(str) {
+//   function check(obj) {
+//       return ['a','i','u','e','o'].indexOf(str.charAt(obj)) == -1 ? check(obj + 1) : obj;
+//   }
+
+//   return str.substr(check(0)).concat((check(0) === 0 ? 'w' : str.substr(0, check(0))) + 'ay');
+// }
+
+// ADVANCED SOLUTION
+// function translatePigLatin(str) {
+//   var strArr = [];
+//   var tmpChar;
+
+//   // check if the char is consonant using RegEx
+  function isConsonant(char) {
+      return !/[aeiou]/.test(char);
+  }
+
+  // return initial str + "way" if it starts with vowel
+  // if not - convert str to array
+  if (!isConsonant(str.charAt(0)))
+      return str + "way";
+  else
+      strArr = str.split("");
+
+  // push all consonats to the end of the array
+  while (isConsonant(strArr[0])) {
+      tmpChar = strArr.shift();
+      strArr.push(tmpChar);
+  }
+// convert array to string and concatenate "ay" at the end  
+return strArr.join("")+"ay";
+}
+
