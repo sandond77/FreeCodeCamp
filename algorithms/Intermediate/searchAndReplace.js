1d572522a2fbceca1f528fd768e212aa96c82769
sandond77 <sandu@ucdavis.edu>
2018-08-25 14:54:05 -0700

    added given solution for searchAndReplace.js

--- algorithms/Intermediate/searchAndReplace.js
+++ algorithms/Intermediate/searchAndReplace.js
@@ -28,22 +28,81 @@
 // 4. Use array.splice() to sub out the words
 
 // My Solution
-function myReplace(str, before, after) {
-	str = str.split(" ");
-	console.log(str)
-	let index = str.indexOf(before);
+// function myReplace(str, before, after) {
+// 	str = str.split(" "); //this will create an array from the string, with the spaces seperating each element
+// 	console.log(str)
+// 	let index = str.indexOf(before);
 
-	if(before[0] === before[0].toUpperCase()){ //this checks to see if the first letter is capitalized or not
-		let letter = after[0].toUpperCase(); 
-		after = letter + after.slice(1) // this will restructure out string with a capitalized letter 
-		console.log(after) 
-	}
+// 	if(before[0] === before[0].toUpperCase()){ //this checks to see if the first letter is capitalized or not
+// 		let letter = after[0].toUpperCase(); 
+// 		after = letter + after.slice(1) // this will restructure out string with a capitalized letter 
+// 		console.log(after) 
+// 	}
 
-	str.splice(index,1,after)
-	str = str.join(" ");
-	console.log(index)
-	console.log(str)
-	return str;
-  }
+// 	str.splice(index,1,after) //this will remove the current words at the given index and splice in the new word
+// 	str = str.join(" "); //this will join our array back together with spaces
+// 	console.log(index)
+// 	console.log(str)
+// 	return str;
+//   }
   
-  myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped");
+//   myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped");
+
+
+// BASIC SOLUTION
+// function myReplace(str, before, after) {
+// 	// Find index where before is on string
+// 	var index = str.indexOf(before);
+// 	// Check to see if the first letter is uppercase or not
+// 	if (str[index] === str[index].toUpperCase()) {
+// 	  // Change the after word to be capitalized before we use it.
+// 	  after = after.charAt(0).toUpperCase() + after.slice(1);
+// 	}
+// 	// Now replace the original str with the edited one.
+// 	str = str.replace(before, after);
+  
+// 	return str;
+//   }
+
+// INTERMEDIATE SOLUTION
+// function myReplace(str, before, after) {
+// 	//Create a regular expression object
+// 	  var re = new RegExp(before,"gi");
+// 	//Check whether the first letter is uppercase or not
+// 	  if(/[A-Z]/.test(before[0])){
+// 	  //Change the word to be capitalized
+// 		after = after.charAt(0).toUpperCase()+after.slice(1);
+// 		 }
+// 		 //Replace the original word with new one
+// 	  var  newStr =  str.replace(re,after);
+	
+// 	 return newStr;
+// }
+	
+
+// ADVANCED SOLUTION
+// function myReplace(str, before, after) {
+
+//     // create a function that will change the casing of any number of letter in parameter "target"
+//     // matching parameter "source"
+//     function applyCasing(source, target) {
+//         // split the source and target strings to array of letters
+//         var targetArr = target.split("");
+//         var sourceArr = source.split("");
+//         // iterate through all the items of sourceArr and targetArr arrays till loop hits the end of shortest array
+//         for (var i = 0; i < Math.min(targetArr.length, sourceArr.length); i++){
+//             // find out the casing of every letter from sourceArr using regular expression
+//             // if sourceArr[i] is upper case then convert targetArr[i] to upper case
+//             if (/[A-Z]/.test(sourceArr[i])) {
+//                 targetArr[i] = targetArr[i].toUpperCase();
+//             }
+//             // if sourceArr[i] is not upper case then convert targetArr[i] to lower case
+//             else targetArr[i] = targetArr[i].toLowerCase();
+//         }
+//         // join modified targetArr to string and return
+//         return (targetArr.join(""));
+//     }
+
+//     // replace "before" with "after" with "before"-casing
+//     return str.replace(before, applyCasing(before, after));
+}
