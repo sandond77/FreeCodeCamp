070f1173f739ca0b38d97dabbe4992c7bf18f54e
sandond77 <sandu@ucdavis.edu>
2018-08-25 21:30:36 -0700

    added given solutions to DNAPair.js

--- algorithms/Intermediate/DNAPair.js
+++ algorithms/Intermediate/DNAPair.js
@@ -56,3 +56,55 @@ function pairElement(str) {
 pairElement("ATCGA") //should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
 pairElement("TTGAG") //should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
 pairElement("CTCTA") //should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
+
+
+// BASIC SOLUTION #1
+//     function pairElement(str) {
+//       // Return each strand as an array of two elements, the original and the pair.
+//       var paired = [];
+
+//       // Function to check with strand to pair.
+//       var search = function(char) {
+//         switch (char) {
+//           case 'A':
+//             paired.push(['A', 'T']);
+//             break;
+//           case 'T':
+//             paired.push(['T', 'A']);
+//             break;
+//           case 'C':
+//             paired.push(['C', 'G']);
+//             break;
+//           case 'G':
+//             paired.push(['G', 'C']);
+//             break;
+//         }
+//       };
+
+//       // Loops through the input and pair.
+//       for (var i = 0; i < str.length; i++) {
+//         search(str[i]);
+//       }
+
+//       return paired;
+//     }
+
+//     // test here
+//     pairElement("GCG")
+
+// INTERMEDIATE SOLUTION #2
+//     function pairElement(str) {
+//       //define a map object with all pair possibilities 
+//       var map = {T:'A', A:'T', G:'C', C:'G'};
+//       //split str into a char Array
+//       strArr = str.split('');
+//       //replace each Array item with a 2d Array using map
+//       for (var i=0;i<strArr.length;i++){
+//         strArr[i]=[strArr[i], map[strArr[i]]];
+//       }
+//      return strArr;
+//     }
+
+//     // test here
+//     pairElement("GCG");
+
