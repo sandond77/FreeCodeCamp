01a69c52dcd9cc3bafdfd485168d55694be58b78
sandond77 <sandu@ucdavis.edu>
2018-08-25 21:28:04 -0700

    added my solution to DNApair.js

--- algorithms/Intermediate/DNAPair.js
+++ algorithms/Intermediate/DNAPair.js
@@ -26,8 +26,33 @@
 // 3. Based of the element, push the corresponding base pair
 
 function pairElement(str) {
-	
+	str = str.split("");
+
+	for (var i = 0; i < str.length; i++) {
+		switch(str[i]){
+			case 'A':
+				str[i] = str[i].split(''); 
+				str[i].push('T');
+				break;
+			case 'T':
+				str[i] = str[i].split('');
+				str[i].push('A');
+				break;
+			case 'C':
+				str[i] = str[i].split('');
+				str[i].push('G');
+				break;
+			case 'G':
+				str[i] = str[i].split('');
+				str[i].push('C');
+				break;
+		} 
+	}
+
+	console.log(str);
   	return str;
 }
 
-pairElement("GCG");
+pairElement("ATCGA") //should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+pairElement("TTGAG") //should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+pairElement("CTCTA") //should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
