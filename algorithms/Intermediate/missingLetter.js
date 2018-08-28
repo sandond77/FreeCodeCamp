bcb1ea7cac5e3b281039c232d4bf65b020a7f88c
sandond77 <sandu@ucdavis.edu>
2018-08-27 20:24:37 -0700

    Added base code/prompt/test code for missingLetter.js

---
+++ algorithms/Intermediate/missingLetter.js
@@ -0,0 +1,26 @@
+// Intermediate Algorithm Scripting: Missing letters
+// Find the missing letter in the passed letter range and return it.
+
+// If all letters are present in the range, return undefined.
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+// BASE CODE
+// function fearNotLetter(str) {
+//   return str;
+// }
+
+// fearNotLetter("abce");
+
+// TEST CODE
+// fearNotLetter("abce") //should return "d".
+// fearNotLetter("abcdefghjklmno")// should return "i".
+// fearNotLetter("stvwx")// should return "u".
+// fearNotLetter("bcdf") //should return "e".
+// fearNotLetter("abcdefghijklmnopqrstuvwxyz") //should return undefined.
+
+
+function fearNotLetter(str) {
+  return str;
+}
+
+fearNotLetter("abce");
