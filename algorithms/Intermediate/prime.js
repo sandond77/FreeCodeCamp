009e0fa98a3618d3debd6630b94a42c4b272ca2c
sandond77 <sandu@ucdavis.edu>
2018-08-29 21:04:29 -0700

    added prompt/base code/test code for prime.js

---
+++ algorithms/Intermediate/prime.js
@@ -0,0 +1,25 @@
+// Intermediate Algorithm Scripting: Sum All Primes
+// Sum all the prime numbers up to and including the provided number.
+// A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
+// The provided number may not be a prime.
+
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+
+// BASE CODE
+// function sumPrimes(num) {
+//   return num;
+// }
+
+// sumPrimes(10);
+
+// TEST CODE
+// sumPrimes(10) //should return a number.
+// sumPrimes(10) //should return 17.
+// sumPrimes(977) //should return 73156.
+
+function sumPrimes(num) {
+  return num;
+}
+
+sumPrimes(10);
