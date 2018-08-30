282ffb5019944c8edd075bbc41a348cb6616cfd5
sandond77 <sandu@ucdavis.edu>
2018-08-29 21:34:22 -0700

    added prime.js solutions

--- algorithms/Intermediate/prime.js
+++ algorithms/Intermediate/prime.js
@@ -2,6 +2,7 @@
 // Sum all the prime numbers up to and including the provided number.
 // A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
 // The provided number may not be a prime.
+// 1 is not a prime number
 
 // Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
 
@@ -51,3 +52,85 @@ function sumPrimes(num) {
 
 sumPrimes(10);
 sumPrimes(977) //should return 73156.
+
+
+// BASIC SOLUTION
+// function sumPrimes(num) {
+//   var res = 0;
+
+//   // Function to get the primes up to max in an array
+//   function getPrimes(max) {
+//     var sieve = [];
+//     var i;
+//     var j;
+//     var primes = [];
+//     for (i = 2; i <= max; ++i) {
+//       if (!sieve[i]) {
+//         // i has not been marked -- it is prime
+//         primes.push(i);
+//         for (j = i << 1; j <= max; j += i) {
+//           sieve[j] = true;
+//         }
+//       }
+//     }
+
+//     return primes;
+//   }
+
+//   // Add the primes
+//   var primes = getPrimes(num);
+//   for (var p = 0; p < primes.length; p++) {
+//     res += primes[p];
+//   }
+
+//   return res;
+// }
+
+
+// INTERMEDIATE SOLUTION
+// function sumPrimes(num) {
+//   // function to check if the number presented is prime
+//   function isPrime(number){
+//       for (i = 2; i <= number; i++){
+//           if(number % i === 0 && number!= i){
+//           // return true if it is divisible by any number that is not itself.
+//              return false;
+//           }
+//        }
+//        // if it passes the for loops conditions it is a prime
+//       return true;
+//   }
+//   // 1 is not a prime, so return nothing, also stops the recursive calls.
+//   if (num === 1){
+//     return 0;
+//   }
+//   // Check if your number is not prime
+//   if(isPrime(num) === false){
+//   // for non primes check the next number down from your maximum number, do not add anything to your answer
+//     return sumPrimes(num - 1);
+//   }
+
+//   // Check if your number is prime
+//   if(isPrime(num) === true){
+//   // for primes add that number to the next number in the sequence through a recursive call to our sumPrimes function.
+//     return num + sumPrimes(num - 1);
+//   }
+// }
+
+// ADVANCED SOLUTION
+// function sumPrimes(num) {
+//   // step 1	
+//   let arr = Array.from({length: num+1}, (v, k) => k).slice(2); 
+//   // step 2
+//   let onlyPrimes = arr.filter( (n) => { 
+//     let m = n-1;
+//     while (m > 1 && m >= Math.sqrt(n)) { 
+//       if ((n % m) === 0) 
+//         return false;
+//         m--;
+//     }
+//       return true;
+//   });
+//   // step 3
+//   return onlyPrimes.reduce((a,b) => a+b); 
+// }
