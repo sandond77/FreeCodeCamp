d8b37b5fe742d62ef1e5e4c02e9549fcd3a61e3a
sandond77 <sandu@ucdavis.edu>
2018-08-30 21:13:17 -0700

    added given solutions for smallestCM.js

--- algorithms/Intermediate/smallestCM.js
+++ algorithms/Intermediate/smallestCM.js
@@ -63,3 +63,105 @@ smallestCommons([5, 1]) // should return 60.
 smallestCommons([2, 10]) // should return 2520.
 smallestCommons([1, 13])  //should return 360360.
 smallestCommons([23, 18]) //should return 6056820.
+
+
+// BASIC SOLUTION //slow solution. takes many iterations due to the nesting
+// function smallestCommons(arr) {
+//   // Sort array from greater to lowest
+//   // This line of code was from Adam Doyle (http://github.com/Adoyle2014)
+//   arr.sort(function(a, b) {
+//     return b - a;
+//   });
+
+//   // Create new array and add all values from greater to smaller from the
+//   // original array.
+//   var newArr = [];
+//   for (var i = arr[0]; i >= arr[1]; i--) {
+//     newArr.push(i);
+//   }
+
+//   // Variables needed declared outside the loops.
+//   var quot = 0;
+//   var loop = 1;
+//   var n;
+
+//   // Run code while n is not the same as the array length.
+//   do {
+//     quot = newArr[0] * loop * newArr[1];
+//     for (n = 2; n < newArr.length; n++) {
+//       if (quot % newArr[n] !== 0) {
+//         break;
+//       }
+//     }
+
+//     loop++;
+//   } while (n !== newArr.length);
+
+//   return quot;
+// }
+
+// INTERMEDIATE SOLUTION
+// function smallestCommons(arr) {
+//     var range = [];
+//     for (var i = Math.max(arr[0], arr[1]); i >= Math.min(arr[0], arr[1]); i--) {
+//     range.push(i);
+//     }
+
+//     // can use reduce() in place of this block
+//     var lcm = range[0];
+//     for (i = 1; i < range.length; i++) {
+//     var GCD = gcd(lcm, range[i]);
+//     lcm = (lcm * range[i]) / GCD;
+//     }
+//     return lcm;
+
+//     function gcd(x, y) {    // Implements the Euclidean Algorithm
+//     if (y === 0)
+//         return x;
+//     else
+//         return gcd(y, x%y);
+//     }
+// }
+
+// ADVANCED SOLUTION
+// function smallestCommons(arr) {
+
+//   // range
+//   let min = Math.min.apply(null, arr);
+//   let max = Math.max.apply(null, arr);
+
+//   let smallestCommon = lcm(min, min + 1);
+
+//   while(min < max) {
+//     min++;
+//     smallestCommon = lcm(smallestCommon, min);
+//   }
+
+//   return smallestCommon;
+// }
+
+// *
+//  * Calculates Greatest Common Divisor
+//  * of two nubers using Euclidean algorithm
+//  * https://en.wikipedia.org/wiki/Euclidean_algorithm
+ 
+// function gcd(a, b) {
+//   while (b > 0) {
+//     let tmp = a;
+//     a = b;
+//     b = tmp % b;
+//   }
+//   return a;
+// }
+
+// /**
+//  * Calculates Least Common Multiple
+//  * for two numbers utilising GCD
+//  */
+// function lcm(a, b) {
+//   return (a * b / gcd(a, b));
+// }
+
+
+// // test here
+// smallestCommons([1,5]);
