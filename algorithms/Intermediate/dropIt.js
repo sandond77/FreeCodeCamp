0b1735b557cd51fdab0e32dcd30ea6c361a8ca7a
sandond77 <sandu@ucdavis.edu>
2018-08-31 15:07:56 -0700

    added solutions for dropIt.js

--- algorithms/Intermediate/dropIt.js
+++ algorithms/Intermediate/dropIt.js
@@ -41,3 +41,32 @@ dropElements([1, 2, 3], function(n) {return n > 0;}) //should return [1, 2, 3].
 dropElements([1, 2, 3, 4], function(n) {return n > 5;}) //should return [].
 dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;})// should return [7, 4].
 dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) //should return [3, 9, 2].
+
+
+
+// BASIC SOLUTION 
+// function dropElements(arr, func) {
+// 	// drop them elements.
+// 	var times = arr.length;
+// 	for (var i = 0; i < times; i++) {
+// 	  if (func(arr[0])) {
+// 		break;
+// 	  } else {
+// 		arr.shift();
+// 	  }
+// 	}
+// 	return arr;
+// }
+
+// INTERMEDIATE SOLUTION
+// function dropElements(arr, func) {
+// 	return arr.slice(arr.findIndex(func) >= 0 ? arr.findIndex(func): arr.length, arr.length);
+// }
+
+// ADVANCED SOLUTION
+// function dropElements(arr, func) {
+// 	while(arr.length > 0 && !func(arr[0])) {
+// 	  arr.shift();
+// 	}
+// 	return arr;
+// }
