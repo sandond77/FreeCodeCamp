c115fb63a03b9c05cfdcdb2ba23a003a03b355f9
sandond77 <sandu@ucdavis.edu>
2018-08-31 22:16:27 -0700

    added solutions for flatten.js

--- algorithms/Intermediate/flatten.js
+++ algorithms/Intermediate/flatten.js
@@ -40,4 +40,52 @@
 //   	return arr;
 // }
 
-steamrollArray([1, [2], [3, [[4]]]]);
+// steamrollArray([1, [2], [3, [[4]]]]);
+
+// BASIC SOLUTION
+// function steamrollArray(arr) {
+// 	var flattenedArray = [];
+  
+// 	// Create function that adds an element if it is not an array.
+// 	// If it is an array, then loops through it and uses recursion on that array.
+// 	var flatten = function(arg) {
+// 	  if (!Array.isArray(arg)) {
+// 		flattenedArray.push(arg);
+// 	  } else {
+// 		for (var a in arg) {
+// 		  flatten(arg[a]);
+// 		}
+// 	  }
+// 	};
+  
+// 	// Call the function for each element in the array
+// 	arr.forEach(flatten);
+// 	return flattenedArray;
+//   }
+  
+//   // test here
+//   steamrollArray([1, [2], [3, [[4]]]]);
+
+// IMMEDIATE SOLUTION
+// function steamrollArray(arr) {
+// 	let flat = [].concat(...arr);
+// 	return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
+//   }
+  
+//   flattenArray([1, [2], [3, [[4]]]]);
+
+// ADVANCED SOLUTION
+// function steamrollArray(arr) {
+// 	return arr.toString()
+// 	  .replace(',,', ',')       // "1,2,,3" => "1,2,3"
+// 	  .split(',')               // ['1','2','3']
+// 	  .map(function(v) {
+// 		if (v == '[object Object]') { // bring back empty objects
+// 		  return {};
+// 		} else if (isNaN(v)) {        // if not a number (string)
+// 		  return v;
+// 		} else {
+// 		  return parseInt(v);         // if a number in a string, convert it
+// 		}
+// 	  });
+// }
