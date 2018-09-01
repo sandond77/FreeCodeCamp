1c2796502abd457c1c81f21e3cbb0b40206d9f10
sandond77 <sandu@ucdavis.edu>
2018-08-31 22:35:30 -0700

    added my solution for binary.js

--- algorithms/Intermediate/binary.js
+++ algorithms/Intermediate/binary.js
@@ -20,3 +20,19 @@
 // 2. Turn binary into uninumber using parseInt() w/ base 2
 // 3. Turn uninumber into the corresponding letter w/ string.fromCharCode()
 // 4. Join array back into string and return solution
+
+// MY SOLUTION
+function binaryAgent(str) {
+	str = str.split(' ');
+	
+	for (let i=0; i < str.length; i++){
+		str[i] = parseInt(str[i], 2)
+		str[i] = String.fromCharCode(str[i]);
+	}
+
+	str = str.join('')
+	console.log(str);
+	return str;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
