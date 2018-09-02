eb323fd678ea623897c62ddff71dba07cb6e15e7
sandond77 <sandu@ucdavis.edu>
2018-09-01 23:18:15 -0700

    added my solution for everythingTrue.js

--- algorithms/Intermediate/everythingTrue.js
+++ algorithms/Intermediate/everythingTrue.js
@@ -24,12 +24,13 @@
 // truthCheck([{"single": "double"}, {"single": undefined}], "single")  //should return false
 // truthCheck([{"single": "double"}, {"single": NaN}], "single")  //should return false
 
+// My Solution
 function truthCheck(collection, pre) { 
 	let status = true;
 
 	for (var i = 0; i < collection.length; i++) {
-		if (!collection[i][pre]) {
-			console.log(collection[i][pre])
+		// console.log(collection[i][pre])
+		if (!collection[i][pre]) { //if this does not exist, we set out flag variable status to false
 			status = false
 		}
 	}
@@ -39,5 +40,12 @@ function truthCheck(collection, pre) {
   return status;
 }
 
-truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
-// truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")  //should return false.
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")  //should return true.
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")  //should return false.
+truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age")  //should return false.
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat")  //should return false
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat")  //should return true
+truthCheck([{"single": "yes"}], "single")  //should return true
+truthCheck([{"single": ""}, {"single": "double"}], "single")  //should return false
+truthCheck([{"single": "double"}, {"single": undefined}], "single")  //should return false
+truthCheck([{"single": "double"}, {"single": NaN}], "single")  //should return false
