99df2c1e346e4df05360704443ee2bb904b358a0
sandond77 <sandu@ucdavis.edu>
2018-09-02 20:19:48 -0700

    added my solution for debris.js

--- algorithms/intermediate/debris.js
+++ algorithms/intermediate/debris.js
@@ -35,7 +35,7 @@
 // 3. Plug into numbers and return the orbital period
 
 
-
+// MY SOLUTION
 function orbitalPeriod(arr) {
 	const GM = 398600.4418;
 	const earthRadius = 6367.4447;
@@ -55,3 +55,63 @@ function orbitalPeriod(arr) {
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]) //should return [{name: "sputnik", orbitalPeriod: 86400}].
 orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]) //should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
+
+
+
+// BASIC SOLUTION
+// function orbitalPeriod(arr) {
+//   var GM = 398600.4418;
+//   var earthRadius = 6367.4447;
+//   var a = 2 * Math.PI;
+//   var newArr = [];
+//   var getOrbPeriod = function(obj) {
+//     var c = Math.pow(earthRadius + obj.avgAlt, 3);
+//     var b = Math.sqrt(c / GM);
+//     var orbPeriod = Math.round(a * b);
+//     delete obj.avgAlt;
+//     obj.orbitalPeriod = orbPeriod;
+//     return obj;
+//   };
+
+//   for (var elem in arr) {
+//     newArr.push(getOrbPeriod(arr[elem]));
+//   }
+
+//   return newArr;
+// }
+
+// INTERMEDIATE SOLUTION
+// function orbitalPeriod(arr) {
+//   var GM = 398600.4418;
+//   var earthRadius = 6367.4447;
+
+//   //Looping through each key in arr object
+//   for(var prop in arr) {
+//     //Rounding off the orbital period value
+//     var orbitalPer = Math.round(2 * Math.PI * Math.sqrt(Math.pow(arr[prop].avgAlt + earthRadius, 3) / GM));
+//     //deleting the avgAlt property
+//     delete arr[prop].avgAlt;
+//     //adding orbitalPeriod property
+//     arr[prop].orbitalPeriod = orbitalPer;
+//   }
+
+//   return arr;
+// }
+
+
+// ADVANCED SOLUTION
+// function orbitalPeriod(arr) {
+//   var GM = 398600.4418;
+//   var earthRadius = 6367.4447;
+
+//   // Loop through each item in the array arr
+//   arr.forEach(function(item) {
+//     // Calculate the Orbital period value
+//     var tmp = Math.round(2 * Math.PI * Math.sqrt(Math.pow(earthRadius + item.avgAlt, 3) / GM));
+//     //Delete the avgAlt property
+//     delete item.avgAlt;
+//     //Add orbitalPeriod property
+//     item.orbitalPeriod = tmp;
+//   });
+//   return arr;
+// }
