35f753fcbb883df452a52f55d840aa861356a943
sandond77 <sandu@ucdavis.edu>
2018-09-04 21:29:58 -0700

    added my code walkthrough for palindrome.js

--- algorithms/algorithms and data structures/palindrome.js
+++ algorithms/algorithms and data structures/palindrome.js
@@ -48,11 +48,11 @@ function palindrome(str) {
 	// str = str.toLowerCase();
 	// str = str.split('');
 	
-	str = str.replace(regex, '').toLowerCase().split('');
-	console.log(str);
-	console.log(str.reverse())
+	str = str.replace(regex, '').toLowerCase().split('') // regex is used to remove the extra spaces from string; turns all to lower case; turns the string to an array
+	// console.log(str);
+	// console.log(str.reverse())
 
-	if (str.join() === str.reverse().join()) {
+	if (str.join() === str.reverse().join()) { //we need an array to use the reverse() method and to rejoin it to compare the strings
 		console.log(true)
 		return true
 	} else {
@@ -61,4 +61,15 @@ function palindrome(str) {
 	}
 }
 
-palindrome("1 eye for of 1 eye.")
+palindrome("eye") //should return true.
+palindrome("_eye") //should return true.
+palindrome("race car") //should return true.
+palindrome("not a palindrome") //should return false.
+palindrome("A man, a plan, a canal. Panama") //should return true.
+palindrome("never odd or even")// should return true.
+palindrome("nope") //should return false.
+palindrome("almostomla")// should return false.
+palindrome("My age is 0, 0 si ega ym.") //should return true.
+palindrome("1 eye for of 1 eye.")// should return false.
+palindrome("0_0 (: /-\ :) 0-0")// should return true.
+palindrome("five|\_/|four") //should return false
