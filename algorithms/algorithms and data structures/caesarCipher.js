cef36750864da841bf3676c7277b17a408834b37
sandond77 <sandu@ucdavis.edu>
2018-09-08 21:22:35 -0700

    added solutions for caesarCipher.js

--- algorithms/algorithms and data structures/caesarCipher.js
+++ algorithms/algorithms and data structures/caesarCipher.js
@@ -96,3 +96,51 @@ function rot13(str) { // LBH QVQ VG!
 }
 
 rot13("SERR PBQR PNZC") //should decode to FREE CODE CAMP
+
+
+
+GIVEN SOLUTION #1
+function rot13(str) {
+	// Split str into a character array
+	return str.split('')
+	// Iterate over each character in the array
+	  .map.call(str, function(char) {
+		// Convert char to a character code
+		x = char.charCodeAt(0);
+		// Checks if character lies between A-Z
+		if (x < 65 || x > 90) {
+		  return String.fromCharCode(x);  // Return un-converted character
+		}
+		//N = ASCII 78, if the character code is less than 78, shift forward 13 places
+		else if (x < 78) {
+		  return String.fromCharCode(x + 13);
+		}
+		// Otherwise shift the character 13 places backward
+		return String.fromCharCode(x - 13);
+	  }).join('');  // Rejoin the array into a string
+}
+
+// GIVEN SOLUTION #2
+// function rot13(str) {
+// 	var rotCharArray = [];
+// 	var regEx = /[A-Z]/ ;
+// 	str = str.split("");
+// 	for (var x in str) {
+// 	  if (regEx.test(str[x])) {
+// 		// A more general approach
+// 		// possible because of modular arithmetic
+// 		// and cyclic nature of rot13 transform
+// 		rotCharArray.push((str[x].charCodeAt() - 65 + 13) % 26 + 65);
+// 	  } else {
+// 		rotCharArray.push(str[x].charCodeAt());
+// 	  }
+// 	}
+// 	str = String.fromCharCode.apply(String, rotCharArray);
+// 	return str;
+// }
+
+
+// GIVEN SOLUTION #3
+// function rot13(str) { // LBH QVQ VG!
+// 	return str.replace(/[A-Z]/g, L => String.fromCharCode((L.charCodeAt(0) % 26) + 65));
+// }
